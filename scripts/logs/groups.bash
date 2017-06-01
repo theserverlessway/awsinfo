3551016e9c07fae9317f7d55c5e474b32c492270
Florian Motlik <flomotlik@gmail.com>
2017-06-01 10:02:54 +0200

    Add Logs Groups command

---
+++ scripts/logs/groups.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+QUERY="logGroups[].[logGroupName]"
+
+if [[ $# -gt 1 ]]; then
+    echo "Plase provide only one argument to match"
+    exit 1
+fi
+
+if [[ $# == 1 ]]; then
+  QUERY="logGroups[?contains(logGroupName,'$1')].[logGroupName]"
+fi
+
+aws logs describe-log-groups --query "$QUERY" --output text

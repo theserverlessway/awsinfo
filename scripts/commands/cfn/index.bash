a03cf57beb19f7bd5543ae986f84d78e650293ce
Florian Motlik <flomotlik@gmail.com>
2017-06-07 16:04:39 +0200

    Name columns for CFN command

--- scripts/commands/cfn/index.bash
+++ scripts/commands/cfn/index.bash
@@ -13,4 +13,4 @@ if [[ $# == 1 ]]; then
   FILTER_QUERY="?contains(StackName,'$1')"
 fi
 
-awscli cloudformation describe-stacks --output table --query "sort_by(Stacks,&StackName)[$FILTER_QUERY].{Stack:StackName,StackCreation:CreationTime,StackUpdate:LastUpdatedTime,Status:StackStatus}"
+awscli cloudformation describe-stacks --output table --query "sort_by(Stacks,&StackName)[$FILTER_QUERY].{\"1.Name\":StackName,\"2.Status\":StackStatus,\"3.CreationTime\":CreationTime,\"4.LastUpdated\":LastUpdatedTime}"

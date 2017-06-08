c7aefb2f022043378829ef475befdd2a137b05db
Florian Motlik <flomotlik@gmail.com>
2017-06-08 17:13:56 +0200

    Remove PrivateDNS on ec2 output

--- scripts/commands/ec2/index.bash
+++ scripts/commands/ec2/index.bash
@@ -11,4 +11,4 @@ if [[ $# -gt 0 ]]; then
     FILTER_QUERY="?$FILTER_NAME||$FILTER_ID"
 fi
 
-awscli ec2 describe-instances --output table --filters Name=instance-state-name,Values=pending,running,shutting-down,stopping,stopped --query "Reservations[].Instances[$FILTER_QUERY][].{\"1.Name\":Tags[?Key=='Name'].Value|[0],\"2.InstanceId\":InstanceId,\"3.Type\":InstanceType,\"4.State\":State.Name,\"5.LaunchTime\":LaunchTime,\"6.AZ\":Placement.AvailabilityZone,\"7.PublicDNS\":PublicDnsName,\"8.PrivateDNS\":PrivateDnsName}"
+awscli ec2 describe-instances --output table --filters Name=instance-state-name,Values=pending,running,shutting-down,stopping,stopped --query "Reservations[].Instances[$FILTER_QUERY][].{\"1.Name\":Tags[?Key=='Name'].Value|[0],\"2.InstanceId\":InstanceId,\"3.Type\":InstanceType,\"4.State\":State.Name,\"5.LaunchTime\":LaunchTime,\"6.AZ\":Placement.AvailabilityZone,\"7.PublicDNS\":PublicDnsName}"

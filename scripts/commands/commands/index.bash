8c415ae41b0f0fca440a49632729881b35bcf0e5
Florian Motlik <flomotlik@gmail.com>
2017-06-15 20:28:46 +0200

    Change commands to list services and commands only when service set

--- scripts/commands/commands/index.bash
+++ scripts/commands/commands/index.bash
@@ -1,3 +1,15 @@
-echo "Available Commands:"
 
-find $DIR/commands -name "*.bash" | sed s/index//g | sed s/.bash//g | awk '{n=split($0,array,"/"); print "  " array[n-1] " " array[n]}' | sort
+if [[ $# -gt 0 ]]
+then
+    if [[ -d "$DIR/commands/$1" ]]
+    then
+        echosuccess Available Commands:
+        find $DIR/commands/$1 -name "*.bash" | sed s/index//g | sed s/.bash//g | awk '{n=split($0,array,"/"); print "  " array[n-1] " " array[n]}' | sort
+    else
+        echo Service "$1" is not supported
+    fi
+else
+    echosuccess Supported Services:
+    ls $DIR/commands | sort | sed 's/^/  /'
+fi
+
--- scripts/commands/commands/index.md
+++ scripts/commands/commands/index.md
@@ -1,2 +1,4 @@
-# `awsinfo commands`
-Lists all available commands.
+# `awsinfo commands [service]`
+
+If no specific service is given as an argument it will list all available services. 
+If you specify a service as the first parameter it will list all commands for that service.

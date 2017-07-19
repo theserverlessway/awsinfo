cd20132a7caef7c0c176cfde5e163a19115fa191
Florian Motlik <flomotlik@gmail.com>
2017-07-19 07:21:30 +0200

    Add Security Groups to EC2 output

--- scripts/commands/ec2/index.bash
+++ scripts/commands/ec2/index.bash
@@ -7,4 +7,4 @@ if [[ $# -gt 0 ]]; then
     FILTER_QUERY="?$(join "||" $FILTER_NAME $FILTER_ID)"
 fi
 
-awscli ec2 describe-instances --output table --filters Name=instance-state-name,Values=pending,running,shutting-down,stopping,stopped --query "Reservations[].Instances[$FILTER_QUERY][].{\"1.Name\":$TAG_NAME,\"2.InstanceId\":InstanceId,\"3.Type\":InstanceType,\"4.State\":State.Name,\"5.LaunchTime\":LaunchTime,\"6.AZ\":Placement.AvailabilityZone,\"7.PublicDNS\":PublicDnsName}"
+awscli ec2 describe-instances --output table --filters Name=instance-state-name,Values=pending,running,shutting-down,stopping,stopped --query "Reservations[].Instances[$FILTER_QUERY][].{\"1.Name\":$TAG_NAME,\"2.InstanceId\":InstanceId,\"3.Type\":InstanceType,\"4.State\":State.Name,\"5.SecurityGroups\":join(', ',NetworkInterfaces[].Groups[].GroupId),\"6.LaunchTime\":LaunchTime,\"7.AZ\":Placement.AvailabilityZone,\"8.PublicDNS\":PublicDnsName}"

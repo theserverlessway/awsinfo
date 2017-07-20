cac8aab6ccbc945a2bcbaab0da34ba69a5f78e80
Florian Motlik <flomotlik@gmail.com>
2017-07-20 08:46:53 +0200

    Print better command error messages and fix Makefile for dev build

--- tests/awsinfo.bash
+++ tests/awsinfo.bash
@@ -2,9 +2,9 @@ awsinfo() {
     run command -v docker
     if [[ $status == 0 ]];
     then
-        docker run -it -v ~/.aws:/root/.aws -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_DEFAULT_REGION -e AWS_DEFAULT_PROFILE -e AWS_CONFIG_FILE flomotlik/awsinfo:master $@
+        docker run -it -v ~/.aws:/root/.aws -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_DEFAULT_REGION -e AWS_DEFAULT_PROFILE -e AWS_CONFIG_FILE flomotlik/awsinfo:dev "$@"
     else
-        ./scripts/awsinfo.bash $@
+        ./scripts/awsinfo.bash "$@"
     fi
 }
 

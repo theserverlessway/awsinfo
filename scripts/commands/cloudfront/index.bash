7d314b0641afc8ad1ad4f7355d4b1c32e40bea41
Florian Motlik <flo@flomotlik.me>
2017-11-12 14:43:20 +0100

    Update commands to use auto_filter

--- scripts/commands/cloudfront/index.bash
+++ scripts/commands/cloudfront/index.bash
@@ -9,4 +9,14 @@ if [[ $# -gt 0 ]]; then
     FILTER_QUERY="?$(join "||" $FILTER_ID $FILTER_DOMAIN $FILTER_STATUS $FILTER_ALIAS)"
 fi
 
-awscli cloudfront list-distributions --output table --query "DistributionList.Items[$FILTER_QUERY].{\"1.Id\": Id, \"2.DomainName\": DomainName, \"3.Status\": Status, \"4.Enabled\":Enabled, \"5.PriceClass\": PriceClass, \"6.Aliases\": (Aliases.Items||[''])|join(',',@), \"7.Origins\": Origins.Quantity, \"8.CacheBehaviors\": CacheBehaviors.Quantity}"
+FILTER=$(auto_filter Id DomainName Status "join(',',Aliases.Items||[''])" -- $@)
+
+awscli cloudfront list-distributions --output table --query "DistributionList.Items[$FILTER].{ \
+  \"1.Id\": Id, \
+  \"2.DomainName\": DomainName, \
+  \"3.Status\": Status, \
+  \"4.Enabled\":Enabled, \
+  \"5.PriceClass\": PriceClass, \
+  \"6.Aliases\": (Aliases.Items||[''])|join(',',@), \
+  \"7.Origins\": Origins.Quantity, \
+  \"8.CacheBehaviors\": CacheBehaviors.Quantity}"
--- scripts/commands/kms/aliases.bash
+++ scripts/commands/kms/aliases.bash
@@ -1,10 +1,3 @@
-FILTER_QUERY=""
+FILTER=$(auto_filter "(TargetKeyId||'')" AliasName -- $@)
 
-if [[ $# -gt 0 ]]; then
-    FILTER_KEY_ID+=$(filter_query "(TargetKeyId||'')" $@)
-    FILTER_NAME+=$(filter_query "AliasName" $@)
-
-    FILTER_QUERY="?$(join "||" $FILTER_KEY_ID $FILTER_NAME)"
-fi
-
-awscli kms list-aliases --output table --query "Aliases[$FILTER_QUERY].{\"1.Name\": AliasName, \"2.Key\": TargetKeyId||'', \"3.AliasArn\": AliasArn}"
+awscli kms list-aliases --output table --query "Aliases[$FILTER].{\"1.Name\": AliasName, \"2.Key\": TargetKeyId||'', \"3.AliasArn\": AliasArn}"

c2d91104d204cf366814f70cb0cb739dd418d464
Florian Motlik <flo@flomotlik.me>
2018-02-07 15:26:29 +0100

    User SerialNumber correctly

--- scripts/commands/iam/mfa.bash
+++ scripts/commands/iam/mfa.bash
@@ -1,7 +1,7 @@
-awscli iam list-virtual-mfa-devices --output table --query "VirtualMFADevices[$(auto_filter User.UserName User.UserId User.Path -- $@)].User.{
-\"1.Arn\":Arn,
-\"2.UserName\":UserName,
-\"3.UserId\":UserId,
-\"4.Path\":Path,
-\"5.PasswordLastUsed\":PasswordLastUsed,
-\"6.CreateDate\":CreateDate}"
+awscli iam list-virtual-mfa-devices --output table --query "VirtualMFADevices[$(auto_filter User.UserName User.UserId User.Path -- $@)].{
+\"1.SerialNumber\":SerialNumber,
+\"2.UserName\":User.UserName,
+\"3.UserId\":User.UserId,
+\"4.Path\":User.Path,
+\"5.PasswordLastUsed\":User.PasswordLastUsed,
+\"6.CreateDate\":User.CreateDate}"

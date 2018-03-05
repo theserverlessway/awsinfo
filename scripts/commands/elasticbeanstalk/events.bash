85a28fa8313a8c874d47f6b373d52416b17df5f7
Florian Motlik <flo@flomotlik.me>
2018-03-05 11:36:10 +0100

    Add ApplicationName to EB events filter

--- scripts/commands/elasticbeanstalk/events.bash
+++ scripts/commands/elasticbeanstalk/events.bash
@@ -1,4 +1,4 @@
-ENVIRONMENTS=$(awscli elasticbeanstalk describe-environments --output text --query "sort_by(Environments,&EnvironmentName)[$(filter EnvironmentName $@)].[EnvironmentName]")
+ENVIRONMENTS=$(awscli elasticbeanstalk describe-environments --output text --query "sort_by(Environments,&EnvironmentName)[$(auto_filter EnvironmentName ApplicationName -- $@)].[EnvironmentName]")
 select_one Environment "$ENVIRONMENTS"
 
 awscli elasticbeanstalk describe-events --environment-name $SELECTED --output table --query "Events[0:50]|reverse(@)[].{\"1.EventDate\":EventDate,\"2.Severity\":Severity,\"3.Message\":Message}"

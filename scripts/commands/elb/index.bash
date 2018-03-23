b8cf68734d5de3b9d516fe9d5be39e845cffa892
Florian Motlik <flo@flomotlik.me>
2018-03-23 13:04:39 +0100

    Support elbv2 by default for elb and move old to old command

--- scripts/commands/elb/index.bash
+++ scripts/commands/elb/index.bash
@@ -1,3 +1,12 @@
-FILTER=$(auto_filter LoadBalancerName VPCId "join('',AvailabilityZones)" "join('',Instances[].InstanceId)" -- $@)
+FILTER=$(auto_filter LoadBalancerName VpcId Scheme "join('',AvailabilityZones[].ZoneName)" "join('',AvailabilityZones[].SubnetId)" "join('',SecurityGroups||[''])" -- $@)
 
-awscli elb describe-load-balancers --query "LoadBalancerDescriptions[$FILTER]" --output table
+awscli elbv2 describe-load-balancers --output table --query "LoadBalancers[$FILTER].{
+  \"1.LoadBalancerName\":LoadBalancerName,
+  \"2.Type\":Type,
+  \"3.Scheme\":Scheme,
+  \"4.VpcId\":VpcId,
+  \"5.AZs\": AvailabilityZones||''|length(@),
+  \"6.SGs\": SecurityGroups||''|length(@),
+  \"7.IPType\":IpAddressType,
+  \"8.DNSName\":DNSName
+  }"

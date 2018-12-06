716eabfecffb3428a25fc7f4fd279ef776b94aa3
Florian Motlik <flo@flomotlik.me>
2018-12-06 17:40:05 +0100

    Automatically load Account id if not given to assume role

--- scripts/commands/assume/role.bash
+++ scripts/commands/assume/role.bash
@@ -1,3 +1,5 @@
+ACCOUNT_ID=""
+ROLE_NAME=""
 while getopts "s:t:d:a:r:" opt; do
     case "$opt" in
         t) TOKEN_CODE="--token-code $OPTARG" ;;
@@ -8,5 +10,17 @@ while getopts "s:t:d:a:r:" opt; do
     esac
 done
 shift $(($OPTIND-1))
+
+if [[ -z "$ROLE_NAME" ]]
+then
+  echo "Please set the Role Name with -r"
+  exit 1
+fi
+
+if [[ -z "$ACCOUNT_ID" ]]
+then
+  ACCOUNT_ID=$(awscli sts get-caller-identity --query "Account" --output text)
+fi
+
 
 awscli sts assume-role --role-arn arn:aws:iam::${ACCOUNT_ID:-}:role/${ROLE_NAME:-} ${SERIAL_NUMBER:-} ${TOKEN_CODE:-} ${MAX_DURATION:-} --role-session-name $ACCOUNT_ID-$ROLE_NAME-$RANDOM --output json | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=" + .AccessKeyId + " AWS_SECRET_ACCESS_KEY=" + .SecretAccessKey + " AWS_SESSION_TOKEN=\"" + .SessionToken + "\" AWS_TOKEN_EXPIRATION=" + .Expiration'

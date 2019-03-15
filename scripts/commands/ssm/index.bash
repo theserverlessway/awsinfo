1f9e6e88ace5b1c7b59ebc96ce77a72d6f6f5fc2
Florian Motlik <flo@flomotlik.me>
2019-03-15 09:15:28 +0100

    Add support for ssm parameters

---
+++ scripts/commands/ssm/index.bash
@@ -0,0 +1,5 @@
+ awscli ssm describe-parameters --output table --query "sort_by(Parameters,&Name)[$(auto_filter Name LastModifiedUser Type -- $@)].{
+  \"1.Name\":Name,
+  \"2.Type\":Type,
+  \"3.Version\":Version,
+  \"4.LastModifiedUser\":LastModifiedUser}"
---
+++ scripts/commands/ssm/parameter.md
@@ -0,0 +1,9 @@
+# `awsinfo ssm parameter [parameter-filter]*`
+
+Get details and the value of a SSM Parameter
+
+## Filter matches against
+
+* Name
+* LastModifiedUser
+* Type

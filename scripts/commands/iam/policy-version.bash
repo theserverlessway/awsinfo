82edd9ff85329f4d2139c0efd8559017c6dddf58
Florian Motlik <flo@flomotlik.me>
2019-03-27 09:58:03 +0100

    Update IAM policy command to json output

--- scripts/commands/iam/policy-version.bash
+++ scripts/commands/iam/policy-version.bash
@@ -8,8 +8,5 @@ POLICY=$SELECTED
 VERSIONS=$(awscli iam list-policy-versions --policy-arn $POLICY  --output text --query "Versions[$(auto_filter VersionId -- $SECOND_RESOURCE)].[VersionId]")
 select_one Version "$VERSIONS"
 
-awscli iam get-policy-version --policy-arn $POLICY --version-id $SELECTED --output table \
-  --query "PolicyVersion.{ \
-    \"1.VersionId\":VersionId, \
-    \"2.IsDefaultVersion\":IsDefaultVersion, \
-    \"3.Document\":Document}"
+awscli iam get-policy-version --policy-arn $POLICY --version-id $SELECTED --output json \
+  --query "PolicyVersion"

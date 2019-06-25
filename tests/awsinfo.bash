6936a96bae402da78a7337c1b7dac3e13571bdfb
Florian Motlik <flo@flomotlik.me>
2019-06-25 14:38:40 +0200

    Fix docker container name in integration tests

--- tests/awsinfo.bash
+++ tests/awsinfo.bash
@@ -2,7 +2,7 @@ awsinfo() {
     run command -v docker
     if [[ $status == 0 ]];
     then
-        docker run -it -v ~/.aws:/root/.aws -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_DEFAULT_REGION -e AWS_DEFAULT_PROFILE -e AWS_CONFIG_FILE flomotlik/awsinfo:dev "$@"
+        docker run -it -v ~/.aws:/root/.aws -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_DEFAULT_REGION -e AWS_DEFAULT_PROFILE -e AWS_CONFIG_FILE theserverlessway/awsinfo:dev "$@"
     else
         ./scripts/awsinfo.bash "$@"
     fi

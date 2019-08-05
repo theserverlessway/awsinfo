00e09c390ff0076a112955160e089913c326facb
Florian Motlik <flo@flomotlik.me>
2019-08-05 10:36:54 +0200

    Add EFS File System command for all info on one file system

---
+++ scripts/commands/efs/file-system.bash
@@ -0,0 +1,7 @@
+split_args "$@"
+
+FILE_SYSTEM_LISTING=$(awscli efs describe-file-systems --output text --query "sort_by(FileSystems,&FileSystemId)[$(auto_filter FileSystemId -- $FIRST_RESOURCE)].[FileSystemId]")
+select_one Stack "$FILE_SYSTEM_LISTING"
+
+awscli efs describe-file-systems --file-system-id $SELECTED --output table \
+  --query "FileSystems[0]"

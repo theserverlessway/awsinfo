fe186c658988797c6e32a9af020aaf85f3d38f0a
Florian Motlik <flo@theserverlessway.com>
2019-10-31 11:31:59 +0100

    add endpoints command and rename from ec2 service

--- scripts/commands/vpc/endpoints-available.bash
+++ scripts/commands/vpc/endpoints-available.bash
@@ -4,9 +4,4 @@ awscli ec2 describe-vpc-endpoint-services --output table \
   --query "ServiceDetails[$FILTER].{ \
     \"1.ServiceName\":ServiceName, \
     \"2.ServiceType\":join(', ', ServiceType[].ServiceType), \
-    \"3.Owner\":Owner, \
-    \"4.BaseEndpointDnsNames\":join(',', BaseEndpointDnsNames), \
-    \"5.PrivateDnsName\":PrivateDnsName, \
-    \"6.AvailabilityZones\":join(', ', AvailabilityZones)
-    \"7.VpcEndpointPolicy\":VpcEndpointPolicySupported, \
-    \"8.AcceptanceRequired\":AcceptanceRequired }"
+    \"3.Owner\":Owner}"

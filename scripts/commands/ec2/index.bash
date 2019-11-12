097e7ad24bf28ca2ed3c26c57c6bf9adf4940111
Florian Motlik <flo@theserverlessway.com>
2019-11-12 11:26:13 +0100

    Fix ECS and EC2 instances output for better visibility

--- scripts/commands/ec2/index.bash
+++ scripts/commands/ec2/index.bash
@@ -7,7 +7,7 @@ while getopts "t" opt; do
 done
 shift $(($OPTIND-1))
 
-SECURITY_GROUPS="join(',',NetworkInterfaces[].Groups[].GroupId)"
+SECURITY_GROUPS="NetworkInterfaces|length(@)"
 FILTER=$(auto_filter "$TAG_NAME" InstanceId InstanceType State.Name Placement.AvailabilityZone "$SECURITY_GROUPS" -- $@)
 
-awscli ec2 describe-instances --output table $EC2_FILTER --query "sort_by(Reservations,&Instances[0].LaunchTime)[].Instances[$FILTER][].{\"1.Name\":$TAG_NAME,\"2.InstanceId\":InstanceId,\"3.Type\":InstanceType,\"4.State\":State.Name,\"5.SecurityGroups\":$SECURITY_GROUPS,\"6.LaunchTime\":LaunchTime,\"7.AZ\":Placement.AvailabilityZone,\"8.PublicDNS\":PublicDnsName, \"9.PrivateDNS\":PrivateDnsName}"
+awscli ec2 describe-instances --output table $EC2_FILTER --query "sort_by(Reservations,&Instances[0].LaunchTime)[].Instances[$FILTER][].{\"1.Name\":$TAG_NAME,\"2.InstanceId\":InstanceId,\"3.Type\":InstanceType,\"4.State\":State.Name,\"5.NetworkInterfaces\":$SECURITY_GROUPS,\"6.LaunchTime\":LaunchTime,\"7.AZ\":Placement.AvailabilityZone,\"8.PublicDNS\":PublicDnsName, \"9.PrivateDNS\":PrivateDnsName}"

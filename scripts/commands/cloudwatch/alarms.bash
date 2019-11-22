9449056e8f3d383e4250307060bacf6dcf02aa4f
Florian Motlik <flo@theserverlessway.com>
2019-11-22 15:49:27 +0100

    Clean up cloudwatch alarms command

--- scripts/commands/cloudwatch/alarms.bash
+++ scripts/commands/cloudwatch/alarms.bash
@@ -1,1 +1,6 @@
-awscli cloudwatch describe-alarms --output table --query "MetricAlarms[$(auto_filter AlarmName StateValue Namespace MetricName -- $@)].{\"1.Name\":AlarmName,\"2.Status\":StateValue,\"3.Metric\":join('/',[Namespace,MetricName]),\"4.Dimension\":Dimensions[].join('/',[Name,Value])|join(',',@),\"5.Comparison\":join('/',[ComparisonOperator,to_string(Threshold)])}"
+awscli cloudwatch describe-alarms --output table --query "MetricAlarms[$(auto_filter AlarmName StateValue Namespace MetricName -- $@)].{
+  \"1.Name\":AlarmName,
+  \"2.Status\":StateValue,
+  \"3.Metric\":join('/',[Namespace||'',MetricName||'']),
+  \"4.Dimension\":Dimensions[].join('/',[Name,Value])|join(',',@),
+  \"5.Comparison\":join('/',[ComparisonOperator,to_string(Threshold)])}"

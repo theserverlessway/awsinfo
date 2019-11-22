26e947d94811d868f6f4b366f3cf3428f76a285a
Florian Motlik <flo@theserverlessway.com>
2019-11-22 15:48:37 +0100

    Improve iam commands

---
+++ scripts/commands/iam/attached-user-policies.md
@@ -0,0 +1,9 @@
+# `awsinfo iam attached-user-policies [filters]*`
+
+List all attached user policy names for a matched IAM user
+
+## Filter matches against
+
+* User Name
+* User Id
+* Path
--- scripts/commands/iam/role.bash
+++ scripts/commands/iam/role.bash
@@ -1,4 +1,4 @@
 ROLES=$(awscli iam list-roles --output text --query "Roles[$(auto_filter RoleName RoleId -- $@)].[RoleName]")
 select_one Role "$ROLES"
 
-awscli iam get-role --role-name $SELECTED --output table --query "Role.{\"1.Name\": RoleName, \"2.Id\": RoleId, \"3.AssumeRolePolicyDocument\": AssumeRolePolicyDocument.Statement}"
+awscli iam get-role --role-name $SELECTED --output table

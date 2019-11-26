acbe247b4a90345d33747159d149259fd815d1f2
Florian Motlik <flo@theserverlessway.com>
2019-11-26 11:35:55 +0100

    Improve Cloudwatch alarms commands

--- scripts/commands/cloudwatch/alarms.bash
+++ scripts/commands/cloudwatch/alarms.bash
@@ -1,6 +1,9 @@
-awscli cloudwatch describe-alarms --output table --query "MetricAlarms[$(auto_filter AlarmName StateValue Namespace MetricName -- $@)].{
+FILTER=$(auto_filter AlarmName StateValue "Namespace||['']" "MetricName||['']" -- $@)
+
+awscli cloudwatch describe-alarms --output table --query "MetricAlarms[$FILTER].{
   \"1.Name\":AlarmName,
   \"2.Status\":StateValue,
   \"3.Metric\":join('/',[Namespace||'',MetricName||'']),
   \"4.Dimension\":Dimensions[].join('/',[Name,Value])|join(',',@),
-  \"5.Comparison\":join('/',[ComparisonOperator,to_string(Threshold)])}"
+  \"5.Comparison/Threshold\":join('/',[ComparisonOperator,to_string(Threshold)]),
+  \"6.Periods/Length\":join('/',[to_string(Period),to_string(EvaluationPeriods)])}"

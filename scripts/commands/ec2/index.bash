430f5fd41461288fccc93186c8b149fef14141fb
Florian Motlik <flo@theserverlessway.com>
2020-03-27 12:01:08 +0100

    Make public and private ips more visible in ec2 index

--- scripts/commands/ec2/index.bash
+++ scripts/commands/ec2/index.bash
@@ -8,6 +8,15 @@ done
 shift $(($OPTIND-1))
 
 SECURITY_GROUPS="NetworkInterfaces|length(@)"
-FILTER=$(auto_filter "$TAG_NAME" InstanceId InstanceType State.Name Placement.AvailabilityZone "$SECURITY_GROUPS" -- $@)
+FILTER=$(auto_filter "$TAG_NAME" InstanceId InstanceType State.Name Placement.AvailabilityZone "$SECURITY_GROUPS" PublicIpAddress PrivateIpAddress -- $@)
 
-awscli ec2 describe-instances --output table $EC2_FILTER --query "sort_by(Reservations,&Instances[0].LaunchTime)[].Instances[$FILTER][].{\"1.Name\":$TAG_NAME,\"2.InstanceId\":InstanceId,\"3.Type\":InstanceType,\"4.State\":State.Name,\"5.NetworkInterfaces\":$SECURITY_GROUPS,\"6.LaunchTime\":LaunchTime,\"7.AZ\":Placement.AvailabilityZone,\"8.PublicDNS\":PublicDnsName, \"9.PrivateDNS\":PrivateDnsName}"
+awscli ec2 describe-instances --output table $EC2_FILTER --query "sort_by(Reservations,&Instances[0].LaunchTime)[].Instances[$FILTER][].{
+  \"1.Name\":$TAG_NAME,
+  \"2.InstanceId\":InstanceId,
+  \"3.Type\":InstanceType,
+  \"4.State\":State.Name,
+  \"5.NetIf\":$SECURITY_GROUPS,
+  \"6.LaunchTime\":LaunchTime,
+  \"7.AZ\":Placement.AvailabilityZone,
+  \"8.PublicIP\":PublicIpAddress,
+  \"9.PrivateIP\":PrivateIpAddress}"

48612942b611a45f342103b2d1ea16634d88d1b3
Florian Motlik <flo@theserverlessway.com>
2020-07-13 10:51:13 +0200

    Fix up some command outputs

--- scripts/commands/es/domain.bash
+++ scripts/commands/es/domain.bash
@@ -4,10 +4,4 @@ DOMAIN_NAMES=$(awscli es list-domain-names --output text --query "sort_by(Domain
 select_one DomainName "$DOMAIN_NAMES"
 
 awscli es describe-elasticsearch-domain --domain-name $SELECTED --output table \
-  --query "DomainStatus.{ \
-    \"1.Name\":DomainName, \
-    \"2.Endpoint\":Endpoint, \
-    \"3.Version\":ElasticsearchVersion, \
-    \"4.Cluster\":ElasticsearchClusterConfig, \
-    \"5.VPCOptions\":VPCOptions||{VPCId: 'None'}, \
-    \"6.EBSOptions\":EBSOptions}"
+  --query "DomainStatus"

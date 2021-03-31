55ad2039b891af034f43b378444605b96075daaa
Florian Motlik <flo@theserverlessway.com>
2021-03-31 10:09:18 +0200

    Show all VPC CIDR Blocks

--- scripts/commands/vpc/index.bash
+++ scripts/commands/vpc/index.bash
@@ -2,7 +2,7 @@ FILTER=$(auto_filter VpcId "$TAG_NAME" "to_string(IsDefault)" -- $@)
 awscli ec2 describe-vpcs --output table --query "Vpcs[$FILTER].{ \
   \"1.Name\":$TAG_NAME, \
   \"2.Id\": VpcId, \
-  \"3.CidrBlock\": CidrBlock, \
+  \"3.CidrBlock\": join(', ',CidrBlockAssociationSet[].CidrBlock), \
   \"4.State\": State, \
   \"5.InstanceTenancy\":InstanceTenancy, \
   \"6.IsDefault\": IsDefault}"

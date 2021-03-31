582634ade77008759bdf0ad927995c8f5e14c2c2
Florian Motlik <flo@theserverlessway.com>
2021-03-31 10:09:26 +0200

    Add ELB listener Certificates command

---
+++ scripts/commands/elb/listener-certificates.bash
@@ -0,0 +1,14 @@
+split_args "$@"
+
+source $CURRENT_COMMAND_DIR/loadbalancer_filter.sh
+FILTER=$(create_filter $FIRST_RESOURCE)
+
+LOAD_BALANCERS=$(awscli elbv2 describe-load-balancers --output text --query "LoadBalancers[$FILTER].[LoadBalancerArn]")
+select_one LoadBalancer "$LOAD_BALANCERS"
+
+LISTENER_FILTER=$(auto_filter "to_string(Port)" Protocol -- $SECOND_RESOURCE)
+
+LISTENERS=$(awscli elbv2 describe-listeners --load-balancer-arn $SELECTED --output text --query "Listeners[$LISTENER_FILTER].[ListenerArn]")
+select_one Listener "$LISTENERS"
+
+awscli elbv2 describe-listener-certificates --listener-arn $SELECTED --output table --query "Certificates"
---
+++ scripts/commands/elb/listener-certificates.md
@@ -0,0 +1,17 @@
+# `awsinfo elb listener-certificates [load-balancer-filter]* -- [listener-filter]*`
+
+List all Load Balancer Listener Certificates
+
+## First filter matches against
+
+* LoadBalancerName
+* VpcId
+* Scheme
+* Availability Zones
+* SubnetIds
+* Security Groups
+
+## Second filter matches against
+
+* Port
+* Protocol

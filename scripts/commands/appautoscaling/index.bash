492855be778f0fe3b3ed7bb93c57d9323884a7ea
Florian Motlik <flo@theserverlessway.com>
2021-04-06 10:10:10 +0200

    Add Resource and dimension filter to appautoscaling

--- scripts/commands/appautoscaling/index.bash
+++ scripts/commands/appautoscaling/index.bash
@@ -1,8 +1,19 @@
+RESOURCE=""
+DIMENSION=""
+
+while getopts "r:d:" opt; do
+  case "$opt" in
+  r) RESOURCE="--resource-id $OPTARG" ;;
+  d) DIMENSION="--scalable-dimension $OPTARG" ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
 split_args "$@"
 
 source $CURRENT_COMMAND_DIR/service_namespace.sh
 
-awscli application-autoscaling describe-scaling-activities --service-namespace $FIRST_RESOURCE --max-items 200 --output table --query "ScalingActivities[$(auto_filter ResourceId ScalableDimension Cause StatusCode StatusMessage -- $SECOND_RESOURCE)].{
+awscli application-autoscaling describe-scaling-activities --service-namespace $FIRST_RESOURCE --max-items 200 $RESOURCE $DIMENSION --output table --query "ScalingActivities[$(auto_filter ResourceId ScalableDimension Cause StatusCode StatusMessage -- $SECOND_RESOURCE)].{
   \"1.StartTime\":StartTime,
   \"2.Resource\":ResourceId,
   \"3.Dimension\":ScalableDimension,
--- scripts/commands/appautoscaling/index.md
+++ scripts/commands/appautoscaling/index.md
@@ -4,6 +4,12 @@ Describe all Application Autoscaling Events for a namespace.
 
 The namespace has to fit one of the supported namespaces, e.g. `ecs`
 
+## Options:
+
+* `-r` limit events to ResourceId
+* `-d` limit events to Dimension
+
+
 ## Filter matches against
 
 * ResourceId 

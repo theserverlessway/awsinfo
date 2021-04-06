d62013efcaad241b123ef4bdd3246285a8f86209
Florian Motlik <flo@theserverlessway.com>
2021-04-06 10:01:05 +0200

    Add Timestamp to appautoscaling

--- scripts/commands/appautoscaling/index.bash
+++ scripts/commands/appautoscaling/index.bash
@@ -3,8 +3,8 @@ split_args "$@"
 source $CURRENT_COMMAND_DIR/service_namespace.sh
 
 awscli application-autoscaling describe-scaling-activities --service-namespace $FIRST_RESOURCE --max-items 200 --output table --query "ScalingActivities[$(auto_filter ResourceId ScalableDimension Cause StatusCode StatusMessage -- $SECOND_RESOURCE)].{
-  \"1.Resource\":ResourceId,
-  \"2.Dimension\":ScalableDimension,
-  \"3.Cause\":Cause,
-  \"4.StatusCode\":StatusCode,
-  \"5.StatusMessage\":StatusMessage}"
+  \"1.StartTime\":StartTime,
+  \"2.Resource\":ResourceId,
+  \"3.Dimension\":ScalableDimension,
+  \"4.Cause\":Cause,
+  \"5.StatusCode\":StatusCode}"

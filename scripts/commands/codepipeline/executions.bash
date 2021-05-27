7092f8bf2fe60094d9f739f6b85e6a5b7471b2e5
Florian Motlik <flo@theserverlessway.com>
2021-05-27 14:04:25 +0200

    Fix Codepipeline executions issue with empty array

--- scripts/commands/codepipeline/executions.bash
+++ scripts/commands/codepipeline/executions.bash
@@ -3,7 +3,7 @@ split_args "$@"
 PIPELINES=$(awscli codepipeline list-pipelines --output text --query "pipelines[$(auto_filter name version -- $FIRST_RESOURCE)].[name]")
 select_one Pipeline "$PIPELINES"
 
-FILTER=$(auto_filter pipelineExecutionId status "join('',sourceRevisions[].revisionId||[])" -- $SECOND_RESOURCE)
+FILTER=$(auto_filter pipelineExecutionId status "join('',sourceRevisions[].revisionId||[''])" -- $SECOND_RESOURCE)
 
 awscli codepipeline list-pipeline-executions --pipeline-name "$SELECTED" --output table --query "pipelineExecutionSummaries[$FILTER].{
   \"1.ExecutionId\":pipelineExecutionId,

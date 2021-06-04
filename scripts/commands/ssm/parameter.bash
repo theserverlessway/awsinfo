119a7f6d96c6a529ea429e9aaa8f697d1d7b045a
Florian Motlik <flo@theserverlessway.com>
2021-06-04 12:08:38 +0200

    Add filter to ssm parameter command

--- scripts/commands/ssm/parameter.bash
+++ scripts/commands/ssm/parameter.bash
@@ -1,4 +1,12 @@
-PARAMETER_LISTING=$(awscli ssm describe-parameters --output text --query "sort_by(Parameters,&Name)[$(auto_filter Name LastModifiedUser Type -- $@)].[Name]")
+FILTER=""
+while getopts "f:" opt; do
+  case "$opt" in
+  f) FILTER="--parameter-filters Key=Name,Option=Contains,Values=$OPTARG" ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+PARAMETER_LISTING=$(awscli ssm describe-parameters  $FILTER --output text --query "sort_by(Parameters,&Name)[$(auto_filter Name LastModifiedUser Type -- $@)].[Name]")
 select_one Parameter "$PARAMETER_LISTING"
 
 awscli ssm get-parameter --name $SELECTED --with-decryption --output table --query "Parameter.{

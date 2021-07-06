d5a42c9471a97703aa6c039ec440e28ffba5d025
Florian Motlik <flo@theserverlessway.com>
2021-07-06 16:46:48 +0200

    Update cost services aggregation

--- scripts/commands/costs/service.bash
+++ scripts/commands/costs/service.bash
@@ -3,7 +3,7 @@ END_DATE=$(awsinfo_date +%Y-%m-%d)
 
 FILTER=$(auto_filter "join('',Keys)" -- $@)
 
-SERVICES=$(awscli ce get-cost-and-usage --time-period Start=$START_DATE,End=$END_DATE --granularity MONTHLY --metrics UNBLENDED_COST --group-by Type=DIMENSION,Key=SERVICE --output text --query "ResultsByTime[].Groups[$FILTER].[join('',Keys)]")
+SERVICES=$(awscli ce get-cost-and-usage --time-period Start=$START_DATE,End=$END_DATE --granularity DAILY --metrics UNBLENDED_COST --group-by Type=DIMENSION,Key=SERVICE --output text --query "ResultsByTime[].Groups[$FILTER].[join('',Keys)]" | uniq)
 
 select_one Service "$SERVICES"
 

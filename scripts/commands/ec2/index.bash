71ea9f94852c4a41efb525e426131c45bc968bfe
Florian Motlik <flo@theserverlessway.com>
2021-08-17 15:25:39 +0200

    Add support for sorting ec2 instances by name

--- scripts/commands/ec2/index.bash
+++ scripts/commands/ec2/index.bash
@@ -3,7 +3,7 @@ source $CURRENT_COMMAND_DIR/terminated-instances.sh
 SECURITY_GROUPS="NetworkInterfaces|length(@)"
 FILTER=$(auto_filter "$TAG_NAME" InstanceId InstanceType State.Name Placement.AvailabilityZone "$SECURITY_GROUPS" PublicIpAddress PrivateIpAddress -- $@)
 
-awscli ec2 describe-instances --output table $EC2_FILTER --query "sort_by(Reservations,&Instances[0].LaunchTime)[].Instances[$FILTER][].{
+awscli ec2 describe-instances --output table $EC2_FILTER --query "sort_by($SORT_BY)[].Instances[$FILTER][].{
   \"1.Name\":$TAG_NAME,
   \"2.InstanceId\":InstanceId,
   \"3.Type\":InstanceType,
--- scripts/commands/ec2/terminated-instances.sh
+++ scripts/commands/ec2/terminated-instances.sh
@@ -1,8 +1,11 @@
 EC2_FILTER="--filter Name=instance-state-name,Values=pending,running"
+SORT_BY="Reservations,&Instances[0].LaunchTime"
 
-while getopts "t" opt; do
+
+while getopts "tn" opt; do
     case "$opt" in
         t) EC2_FILTER="";;
+        n) SORT_BY="Reservations,&Instances[0].Tags[?Key=='Name'].Value|[0]||''";;
     esac
 done
 shift $(($OPTIND-1))

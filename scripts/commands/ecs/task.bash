bdd9d4692f83289f461eb195eab17cc32fc21afc
Florian Motlik <flo@theserverlessway.com>
2021-10-18 15:35:32 +0200

    Use proper listing for tasks

--- scripts/commands/ecs/task.bash
+++ scripts/commands/ecs/task.bash
@@ -21,10 +21,10 @@ select_one Cluster "$CLUSTERS"
 
 CLUSTER=$SELECTED
 
-TASKS=$(awscli ecs list-tasks --output text --cluster $SELECTED $TASK_STATUS --query taskArns)
+TASKS=$(awscli ecs list-tasks --output text --cluster $SELECTED $TASK_STATUS --query taskArns[].[@])
 
 FILTER=$(auto_filter taskArn taskDefinitionArn containerInstanceArn lastStatus group cpu memory launchType capacityProviderName -- $SECOND_RESOURCE)
-FILTERED_TASKS=$(echo "$TASKS" | xargs -rn 99 bash -c "awscli ecs describe-tasks --cluster $SELECTED --query \"tasks[$FILTER].taskArn\" --output text --tasks \$0 \$@")
+FILTERED_TASKS=$(echo "$TASKS" | xargs -rn 99 bash -c "awscli ecs describe-tasks --cluster $SELECTED --query \"tasks[$FILTER].[taskArn]\" --output text --tasks \$0 \$@")
 
 select_one Task "$FILTERED_TASKS"
 

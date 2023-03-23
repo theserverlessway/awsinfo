cb93415b0ce9ba15a3ff1003a6fdc3fc5abc2856
Florian Motlik <flo@theserverlessway.com>
2023-03-23 14:49:13 +0100

    Add InstanceLifecycle to EC2 Index command

--- scripts/commands/ec2/index.bash
+++ scripts/commands/ec2/index.bash
@@ -1,14 +1,13 @@
 source $CURRENT_COMMAND_DIR/terminated-instances.sh
 
-SECURITY_GROUPS="NetworkInterfaces|length(@)"
-FILTER=$(auto_filter "$TAG_NAME" InstanceId InstanceType State.Name Placement.AvailabilityZone "$SECURITY_GROUPS" PublicIpAddress PrivateIpAddress -- $@)
+FILTER=$(auto_filter "$TAG_NAME" InstanceId InstanceType State.Name Placement.AvailabilityZone PublicIpAddress PrivateIpAddress InstanceLifecycle -- $@)
 
 awscli ec2 describe-instances --output table $EC2_FILTER --query "sort_by($SORT_BY)[].Instances[$FILTER][].{
   \"1.Name\":$TAG_NAME,
   \"2.InstanceId\":InstanceId,
   \"3.Type\":InstanceType,
   \"4.State\":State.Name,
-  \"5.NetIf\":$SECURITY_GROUPS,
+  \"5.InstanceLifecycle\":InstanceLifecycle||'on-demand',
   \"6.LaunchTime\":LaunchTime,
   \"7.AZ\":Placement.AvailabilityZone,
   \"8.PublicIP\":PublicIpAddress,
